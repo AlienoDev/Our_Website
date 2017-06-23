70362541e1113f68f3df7fd8cff3f91068e96924
Marco <garcia.j.marco@gmail.com>
2017-06-23 14:51:58 +0200

    svg done

--- public/app/components/team/team.js
+++ public/app/components/team/team.js
@@ -3,10 +3,13 @@
 angular.module("alienodev")
     .component("team", {
         templateUrl: "app/components/team/team.html",
-        controller : Team
+        controller: Team
     })
 
 
 function Team() {
-    
+    new Vivus('test', {
+        type: 'delayed',
+        duration: 200,
+        animTimingFunction: Vivus.EASE });
 }
